11c897703a8eda9fd58d92fd731d0c8e1219fea9
Bin Liang <bingliang711@hotmail.com>
2015-01-28 04:52:31 -0700

    create Car class and call it in main method

---
+++ COMP123-Lesson03/COMP123-Lesson03/Car.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_Lesson03
+{
+    // Car class - blueprint for car objects
+    class Car
+    {
+        //public properties*************************************
+
+        //private properties*************************************
+        private string make;
+        private string model;
+        private string colour;
+        private string VIN;//Vehicle Identification Number
+        private int speed;
+
+        //Constructor*************************************
+        public Car(string make, string model, string colour, string VIN)
+        {
+            //string myString = "Hello";
+
+            //Initialize properties via the constructor
+            this.make = make;
+            this.model = model;
+            this.colour = colour;
+            this.VIN = VIN;
+
+            this.speed = 0;
+
+            this.display();//Display cars property values
+
+        }
+
+        //public methods*************************************
+        //cause the car object to drive forward*******************
+        public void driveForward()
+        {
+            Console.WriteLine("The car is driving forward.");
+            setSpeed(100);
+            Console.WriteLine("Speed: {0}", speed);
+        }
+
+        //private methods*************************************
+        //cause the car to change speed
+        private void setSpeed(int newSpeed)
+        {
+            speed = newSpeed;
+        }
+
+        private void display()
+        {
+            Console.WriteLine("+++++++++++++++++++++++++++++");
+            Console.WriteLine("Make:{0}", this.make);
+            Console.WriteLine("Model:{0}", this.model);
+            Console.WriteLine("Colour:{0}", this.colour);
+            Console.WriteLine("VIN:{0}", this.VIN);
+            Console.WriteLine("+++++++++++++++++++++++++++++");
+
+
+        }
+    }
+}
---
+++ COMP123-Lesson03/COMP123-Lesson03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_Lesson03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("Honda", "Pilot", "Black", "X12345");
+            myCar.driveForward();
+
+
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
